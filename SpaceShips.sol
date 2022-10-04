77c09c2862aa979014a4af8bcdc8e26d8c94d40c
PULSAR302 <pulsar2k10@gmail.com>
2022-10-04 20:51:28 +0100

    Add Contract

--- SpaceShips.sol
+++ SpaceShips.sol
@@ -1,2 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract SpaceShip is ERC1155 {
+    uint256 public constant SpaceShipID = 1;
+
+    constructor() ERC1155("https://ipfs.io/ipfs/bafybeidw47z5awqu3tv4dfg2ooo4ps7co6huw4xhecdh6hzz3ucrcvyyre/{id}.json") {
+        _mint(msg.sender, SpaceShipID, 1, "");
+    }
+}
