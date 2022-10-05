82e5bb26eddcc7b3be7b4d1d18fe1dc576a027ce
PULSAR302 <pulsar2k10@gmail.com>
2022-10-05 17:22:41 +0100

    Add mint - setURI

--- SpaceShips.sol
+++ SpaceShips.sol
@@ -2,11 +2,24 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SpaceShip is ERC1155 {
-    uint256 public constant SpaceShipID = 1;
+contract SpaceShip is ERC1155, Ownable {
+    mapping (uint256 => string) private URIs;
 
-    constructor() ERC1155("https://ipfs.io/ipfs/bafybeidw47z5awqu3tv4dfg2ooo4ps7co6huw4xhecdh6hzz3ucrcvyyre/{id}.json") {
-        _mint(msg.sender, SpaceShipID, 1, "");
+    // https://ipfs.io/ipfs/bafybeidw47z5awqu3tv4dfg2ooo4ps7co6huw4xhecdh6hzz3ucrcvyyre/{id}.json
+
+    constructor() ERC1155("") {}
+
+    function uri(uint256 _TokenID) override public view returns (string memory) {
+      return(URIs[_TokenID]);
+    }
+
+    function setTokenUri(uint256 _TokenID, string memory _uri) public onlyOwner {
+      URIs[_TokenID] = _uri;
+    }
+
+    function mint(address _Recipient, uint256 _TokenID, uint256 _Amount) public onlyOwner {
+      _mint(_Recipient, _TokenID, _Amount, "");
     }
 }
