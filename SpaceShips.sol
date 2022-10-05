525f2aafec44224d923eb9585dfa88bff84340be
PULSAR302 <pulsar2k10@gmail.com>
2022-10-05 23:46:37 +0100

    Change Contract Name

--- SpaceShips.sol
+++ SpaceShips.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SpaceShip is ERC1155, Ownable {
+contract SpaceShipNFT is ERC1155, Ownable {
     mapping (uint256 => string) private URIs;
     mapping (address => uint256) public whitelistedAddresses;
     mapping (uint256 => uint) public expirationDate;
