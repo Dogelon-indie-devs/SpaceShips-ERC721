b759443aa00d29991845be239bd70e13b43457cc
PULSAR302 <pulsar2k10@gmail.com>
2022-10-09 18:10:17 +0100

    Add Generations Array + AddNewGeneration

--- SpaceShips.sol
+++ SpaceShips.sol
@@ -6,8 +6,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DogelonSpaceShipNFT is ERC1155, Ownable {
     mapping (uint256 => string) private URIs;
+    
+    NewGeneration[] public Generations; 
+    struct NewGeneration{
+      uint256 ID;
+      string Uri;
+      uint256 Price;
+      uint256 MaxSupply;
+      bool Unlocked;  
+    }
 
     constructor() ERC1155("") {}
+  
+    function AddNewGeneration(uint256 _ID, string memory _Uri, uint256 _Price, uint256 _MaxSupply, bool _Unlocked) public onlyOwner {
+      Generations.push(NewGeneration(_ID, _Uri, _Price, _MaxSupply, _Unlocked));  
+    }
 
     function uri(uint256 _TokenID) override public view returns (string memory) {
       return(URIs[_TokenID]);
