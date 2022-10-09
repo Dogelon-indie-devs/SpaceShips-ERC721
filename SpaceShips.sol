2b7beade7ffa929062c718059c0b60665c081311
PULSAR302 <pulsar2k10@gmail.com>
2022-10-10 00:06:57 +0100

    Add Some Funcs

--- SpaceShips.sol
+++ SpaceShips.sol
@@ -6,20 +6,40 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DogelonSpaceShipNFT is ERC1155, Ownable {
     mapping (uint256 => string) private URIs;
-    
-    NewGeneration[] public Generations; 
+ 
     struct NewGeneration{
       uint256 ID;
       string Uri;
       uint256 Price;
       uint256 MaxSupply;
       bool Unlocked;  
-    }
+    }  
+    NewGeneration[] private Generations; 
 
     constructor() ERC1155("") {}
   
-    function AddNewGeneration(uint256 _ID, string memory _Uri, uint256 _Price, uint256 _MaxSupply, bool _Unlocked) public onlyOwner {
-      Generations.push(NewGeneration(_ID, _Uri, _Price, _MaxSupply, _Unlocked));  
+    function AddNewGeneration(uint256 _ID, string memory _Uri, uint256 _Price, uint256 _MaxSupply, bool _Unlocked) public onlyOwner { 
+      Generations.push(NewGeneration(_ID, _Uri, _Price, _MaxSupply, _Unlocked));
+     }  
+
+    function UnlockGeneration(uint256 _GenerationID) public onlyOwner {
+      Generations[_GenerationID].Unlocked = true;
+    }
+
+    function LockGeneration(uint256 _GenerationID) public onlyOwner {
+      Generations[_GenerationID].Unlocked = false;
+    }
+
+    function GenerationsCount() public view returns (uint256) {
+      return(Generations.length);
+    }
+
+    function IsGenerationUnlocked(uint256 _GenerationID) public view returns (bool) {
+      return(Generations[_GenerationID].Unlocked);
+    }
+
+    function ExtractGenerationUri(uint256 _GenerationID) public view returns (string memory) {
+      return(Generations[_GenerationID].Uri);
     }
 
     function uri(uint256 _TokenID) override public view returns (string memory) {
