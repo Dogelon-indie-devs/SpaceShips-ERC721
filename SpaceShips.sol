10a6fd922fe5da160db202642f936eaea64375ac
PULSAR302 <pulsar2k10@gmail.com>
2022-10-11 00:41:54 +0100

    Add BluePrintURIS

--- SpaceShips.sol
+++ SpaceShips.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract DogelonSpaceShipNFT is ERC1155, Ownable {
-   string BluePrintURI;
+    string BluePrintURI;
     struct NewGeneration{
       uint256 ID;
       string Uri;
@@ -16,11 +16,12 @@ contract DogelonSpaceShipNFT is ERC1155, Ownable {
     }  
     NewGeneration[] private Generations; 
     mapping (uint256 => bool) MintedTokens;
+    mapping (uint256 => string) BluePrintURIS;
 
     constructor() ERC1155("") {}
   
     function AddNewGeneration(uint256 _ID, string memory _Uri, uint256 _Price, uint256 _MaxSupply, bool _Unlocked) public onlyOwner { 
-      Generations.push(NewGeneration(_ID, _Uri, _Price, _MaxSupply, _Unlocked));
+      Generations.push(NewGeneration(_ID, _Uri, _Price, _MaxSupply, _Unlocked)); 
      }  
 
     function UnlockGeneration(uint256 _GenerationID) public onlyOwner {
@@ -65,13 +66,13 @@ contract DogelonSpaceShipNFT is ERC1155, Ownable {
       if (MintedTokens[_TokenID]) {
         MainURI = string(abi.encodePacked(ExtractGenerationUri(ExtractGenerationIDByTokenID(_TokenID)), Strings.toString(_TokenID), ".json"));   
       } else {
-        MainURI = BluePrintURI;   
+        MainURI = BluePrintURIS[ExtractGenerationIDByTokenID(_TokenID)];   
       }     
       return(MainURI);             
     }
  
-    function SetBluePrintURI(string memory _NewURI) public onlyOwner{
-      BluePrintURI = _NewURI;
+    function SetBluePrintURI(uint256 _GenerationID, string memory _NewURI) public onlyOwner{
+      BluePrintURIS[_GenerationID] = _NewURI; 
     }
 
     function mint(address _Recipient, uint256 _TokenID, uint256 _Amount) public {
