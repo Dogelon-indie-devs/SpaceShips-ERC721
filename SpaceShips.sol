10fc1ed753639bbe76a716a81ea94a60c4ed1bee
PULSAR302 <pulsar2k10@gmail.com>
2022-10-11 00:56:27 +0100

    Add Some Changes [BluePrint]

--- SpaceShips.sol
+++ SpaceShips.sol
@@ -6,22 +6,22 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract DogelonSpaceShipNFT is ERC1155, Ownable {
-    string BluePrintURI;
+
     struct NewGeneration{
       uint256 ID;
       string Uri;
+      string BluePrintUri;
       uint256 Price;
       uint256 MaxSupply;
       bool Unlocked;  
     }  
     NewGeneration[] private Generations; 
     mapping (uint256 => bool) MintedTokens;
-    mapping (uint256 => string) BluePrintURIS;
 
     constructor() ERC1155("") {}
   
-    function AddNewGeneration(uint256 _ID, string memory _Uri, uint256 _Price, uint256 _MaxSupply, bool _Unlocked) public onlyOwner { 
-      Generations.push(NewGeneration(_ID, _Uri, _Price, _MaxSupply, _Unlocked)); 
+    function AddNewGeneration(uint256 _ID, string memory _Uri, string memory _BluePrintUri, uint256 _Price, uint256 _MaxSupply, bool _Unlocked) public onlyOwner { 
+      Generations.push(NewGeneration(_ID, _Uri, _BluePrintUri, _Price, _MaxSupply, _Unlocked)); 
      }  
 
     function UnlockGeneration(uint256 _GenerationID) public onlyOwner {
@@ -66,13 +66,13 @@ contract DogelonSpaceShipNFT is ERC1155, Ownable {
       if (MintedTokens[_TokenID]) {
         MainURI = string(abi.encodePacked(ExtractGenerationUri(ExtractGenerationIDByTokenID(_TokenID)), Strings.toString(_TokenID), ".json"));   
       } else {
-        MainURI = BluePrintURIS[ExtractGenerationIDByTokenID(_TokenID)];   
+        MainURI = Generations[ExtractGenerationIDByTokenID(_TokenID) - 1].BluePrintUri;   
       }     
       return(MainURI);             
     }
  
-    function SetBluePrintURI(uint256 _GenerationID, string memory _NewURI) public onlyOwner{
-      BluePrintURIS[_GenerationID] = _NewURI; 
+    function ChangeBluePrintURI(uint256 _GenerationID, string memory _NewURI) public onlyOwner{
+      Generations[_GenerationID - 1].BluePrintUri = _NewURI; 
     }
 
     function mint(address _Recipient, uint256 _TokenID, uint256 _Amount) public {
