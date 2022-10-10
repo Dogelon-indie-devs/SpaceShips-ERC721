491acbeb695efe1e8a4cbd7206d053098c0be0ad
PULSAR302 <pulsar2k10@gmail.com>
2022-10-10 21:33:31 +0100

    Change uri

--- SpaceShips.sol
+++ SpaceShips.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract DogelonSpaceShipNFT is ERC1155, Ownable {
-    mapping (uint256 => string) private URIs;
  
     struct NewGeneration{
       uint256 ID;
@@ -20,7 +20,6 @@ contract DogelonSpaceShipNFT is ERC1155, Ownable {
   
     function AddNewGeneration(uint256 _ID, string memory _Uri, uint256 _Price, uint256 _MaxSupply, bool _Unlocked) public onlyOwner { 
       Generations.push(NewGeneration(_ID, _Uri, _Price, _MaxSupply, _Unlocked));
-      //setTokenUri(0, _Uri);
      }  
 
     function UnlockGeneration(uint256 _GenerationID) public onlyOwner {
@@ -61,11 +60,9 @@ contract DogelonSpaceShipNFT is ERC1155, Ownable {
     }
 
     function uri(uint256 _TokenID) override public view returns (string memory) {
-      return(URIs[_TokenID]);
-    }
-
-    function setTokenUri(uint256 _TokenID, string memory _uri) public onlyOwner {
-      URIs[_TokenID] = _uri;
+      return(     
+        string(abi.encodePacked(ExtractGenerationUri(ExtractGenerationIDByTokenID(_TokenID)), Strings.toString(_TokenID), ".json"))       
+      );
     }
 
     function mint(address _Recipient, uint256 _TokenID, uint256 _Amount) public onlyOwner {
